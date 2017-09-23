a2e70ec20d30dc2ac38efb8eb5cfe511d19b24b5
Anish Karandikar <anishkny@gmail.com>
2017-09-23 17:44:09 -0400

    Enable newman API tests

---
+++ run-api-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+pwd
+
+## Deploy to Cloud Functions Local Emulator
+which functions
+echo mock-project | functions start
+export DEPLOY_OUTPUT_FILE=`mktemp`
+functions deploy api --trigger-http | tee $DEPLOY_OUTPUT_FILE
+export API_URL=`grep Resource $DEPLOY_OUTPUT_FILE | grep -o 'http://localhost:[^[:space:]]*'`
+echo $API_URL
+
+## Run Postman tests against local deployed API
+which newman
+newman run --global-var="apiUrl=$API_URL" --folder Automated ./api-tests.postman.json
