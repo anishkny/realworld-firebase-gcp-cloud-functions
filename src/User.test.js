e016c3c953eb9dd66e0c921e5109906c786fcb4e
akarandi <anish.karandikar@mathworks.com>
2017-09-26 14:00:41 -0400

    Added key assertions in User.test.js

--- src/User.test.js
+++ src/User.test.js
@@ -9,6 +9,7 @@ var testUser = {
   username: casual.username,
 };
 var loggedInUser = {};
+var expectedUserKeys = ['email', 'token', 'username', 'bio', 'image', ];
 
 before(() => {
   initializeApp();
@@ -22,6 +23,7 @@ describe('Users', () => {
 
   it('create', async() => {
     var createdUser = await User.create(testUser);
+    expect(createdUser.user).to.have.all.keys(['username', 'email', 'password', ]);
     expect(createdUser.user.email).to.equal(testUser.email);
     expect(createdUser.user.password).to.equal(testUser.password);
     expect(createdUser.user.username).to.equal(testUser.username);
@@ -32,11 +34,13 @@ describe('Users', () => {
       email: testUser.email,
       password: testUser.password,
     });
+    expect(loggedInUser.user).to.have.all.keys(expectedUserKeys);
     expect(loggedInUser.user.username).to.equal(testUser.username);
   });
 
   it('validateToken', async() => {
     var validatedUser = await User.validateToken(loggedInUser.user.token);
+    expect(validatedUser.user).to.have.all.keys(expectedUserKeys);
     expect(validatedUser.user.username).to.equal(testUser.username);
   });
 
